b645a1e2151f113763d2c68796b6fa625bcad31a
新家谷一生 <ikkishingeya@shinkekokuisshounoMacBook-Pro.local>
2025-03-01 09:19:31 +0900

    Update game

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router } from "react-router-dom";  // 🔹 GitHub Pages対応
+import { HashRouter } from "react-router-dom";  // 🔹 GitHub Pages対応
 import App from "./App"; // `App` を別ファイルから読み込む
 import "./styles.css"; // スタイル適用
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter basename="/">
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
